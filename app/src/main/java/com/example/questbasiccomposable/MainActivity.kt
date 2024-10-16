2ac9ba5028f489daac33ce565358d92f7c3b305c
hilal555 <muhamadhilal04@gmail.com>
2024-10-16 20:44:30 +0700

    Menambahkan logo UMY

--- app/src/main/java/com/example/questbasiccomposable/MainActivity.kt
+++ app/src/main/java/com/example/questbasiccomposable/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -14,6 +15,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -46,7 +48,11 @@ fun QuestBasicComposable(modifier: Modifier = Modifier ){
             fontWeight = FontWeight.Bold)
         Spacer(modifier = Modifier.padding(2.dp))
         Text("Ini Adalah Halaman Login")
+
         Spacer(modifier = Modifier.padding(7.dp))
+
+        Image(painter = painterResource(id = R.drawable.umy),
+            contentDescription = "")
     }
 }
 
