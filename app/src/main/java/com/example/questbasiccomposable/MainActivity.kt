5bc67787b1b955e2a5cc61bc32ce2aaf5383a859
hilal555 <muhamadhilal04@gmail.com>
2024-10-16 20:35:10 +0700

    Mengubah ukuran text menjadi 75sp

--- app/src/main/java/com/example/questbasiccomposable/MainActivity.kt
+++ app/src/main/java/com/example/questbasiccomposable/MainActivity.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.questbasiccomposable.ui.theme.QuestBasicComposableTheme
 
 class MainActivity : ComponentActivity() {
@@ -38,7 +39,8 @@ fun QuestBasicComposable(modifier: Modifier = Modifier ){
         modifier = Modifier
             .fillMaxSize()
             .padding(top = 50.dp)) {
-        Text("Login")
+        Text("Login",
+            fontSize = 75.sp)
     }
 }
 
